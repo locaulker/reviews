7e574d5a0825a238da3b04e8b9d10df17154883e
Lawrence <locaulker@gmail.com>
2023-01-03 01:12:33 -0500

    finished basic structure setup

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,39 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react"
+import people from "./data"
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa"
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [index, setIndex] = useState(0) // index here means array index
 
-export default Review;
+  /**
+   *  destructuring from a specific array element: people[index]
+   *  Note that array is zero based. Therefore, initial value is set to "0"
+   *  setIndex() points to current index
+   */
+  const { name, job, image, text } = people[index] //
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img className="person-img" src={image} alt={name} />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">surprise Me!</button>
+    </article>
+  )
+}
+
+export default Review
